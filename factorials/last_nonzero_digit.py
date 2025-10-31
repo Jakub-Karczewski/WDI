eb23921c074662734c7c91a9b1155009d502b178
Jakub Karczewski <karczewskij@student.agh.edu.pl>
2025-11-01 00:33:12 +0100

    Update task for finding last nonzero digit of factorial, solution for this task better than calculating almost full number is nearly impossible for 1st year student

--- factorials/last_nonzero_digit.py
+++ factorials/last_nonzero_digit.py
@@ -1,7 +1,6 @@
 import math
 def calc_factorial(num): # function to calculate factorial of a number, can be used for small numbers only
     assert num >= 0, "we cannot calculate factorial of < 0 number"
-
     fact = 1
     for i in range(1, num+1, 1):
         fact *= i
@@ -19,77 +18,31 @@ def get_last_nonzero_digit(N):
         N = N//10 # Removing current last digit, going to the 'left' digit in number
 
 
-def get_length_of_number(n):
-    return math.floor(math.log10(n)) + 1
-
-def last_nonzero_digit_in_factorial(n):
-    # first thing we can observe is that trailing zeros do not affect the last non-zero digit
-    # Also, we still multiply the repeating sequence of last non-zero digits from 1 to 10 of numbers in factorial
+def last_nonzero_in_fact(n, arr):
+    if n <= 9:
+        return arr[n]
     
-    # Unfortunately, this sequence is affected by the number of 2s and 5s we have in our factorial
-    # because they create trailing zeros when paired together, and it will "shift" our sequence
-    # Additionally, we can observe that for every n! number of 2 factors is greater than or equal number of 5 factors
-    # It can be proven using the formula for calculating factors of a prime p (in our case 2 and 5)
-    # in n!, this formula is used in the "zeros_at_the_end.py" file
+    # https://math.stackexchange.com/questions/130352/last-non-zero-digit-of-a-factorial
+    # this task is just impossible, but we will use that formula here xddd
 
-    # Also, we can note that, for example for numbers lesser than 5^3 = 125,
-    # we will create at most 3 trailing zeros in one step (one step is multiplying by next number in factorial),
-    # because we can have at most 3 new factors of 5
-    # This way, we need to store only log5(n) last non-zero digits for n factorial
+    res = (last_nonzero_in_fact(n//5, arr) * arr[n % 10]) % 10
 
-    last_digit = 1
-    factors_of_5 = 0
-    factors_of_2 = 0
+    if ((n % 100)//10) % 2 == 1:
+        res = (4 * res) % 10
+    else:
+        res = (6 * res) % 10
+    return res
 
-    powers_of_5 = []
-    powers_of_2 = []
-    
-    current_power = 5
-    while True:
-        powers_of_5.append(current_power)
-        if current_power * 5 > n:
-            break
-        current_power *= 5
+D = [0 for _ in range(10)]
+res = 1
+for nr in range(1, 10, 1):
+    res *= nr
+    D[nr] = get_last_nonzero_digit(res)
+print("Computed from 1-9:", D)
 
-    current_power = 2
-    while True:
-        powers_of_2.append(current_power)
-        if current_power * 2 > n:
-            break
-        current_power *= 2
 
-    # needed_length = 2 # how many last nonzero digits we need to store currently (it will change with every new power of 5)
-    # current_max_power_of_5 = 25 # we increase needed_length when we reach 25, 125, 625, ...
-
-    for i in range(1, n+1):
-        if i % 2 == 0 or i % 5 == 0:
-            ind1 = 0
-
-            # if our number is a multiply of 2 ^ k it is divisible by all lower powers of 2 as well
-            while ind1 < len(powers_of_2) and i % powers_of_2[ind1] == 0:
-                factors_of_2 += 1
-                ind1 += 1
-
-            # same for 5s
-            ind2 = 0
-            while ind2 < len(powers_of_5) and i % powers_of_5[0] == 0:
-                factors_of_5 += 1
-                ind2 += 1
-
-            # we remove equal number of 2s and 5s to avoid creating trailing zeros
-            min_ind = min(ind1, ind2)
-
-            if ind2 > min_ind:
-                # case when we have more 5s than 2s, we need to remove extra 5s
-                last_digits  = (last_digits % 10 * (((i // 10**min_ind) // 5**(ind2-ind1) ) % 10)) % 10
-            else:
-                last_digits *= (i // 10**min_ind)
-
-            # if i == current_max_power_of_5:
-            #     needed_length += 1
-            #     current_max_power_of_5 *= 5
-
-            assert factors_of_2 >= factors_of_5, "checking our assumption that number of 2s >= number of 5s"
-        else:
-            last_digits  = (last_digits % 10 * (i % 10)) % 10
-
+for i in [27]:
+    fact = calc_factorial(i)
+    last_digit = get_last_nonzero_digit(fact)
+    computed_last_digit = last_nonzero_in_fact(i, D)
+    assert last_digit == computed_last_digit, f"mismatch for {i}! : expected {last_digit}, got {computed_last_digit}"
