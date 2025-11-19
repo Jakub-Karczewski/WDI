b5721b22a52d2d64fee470465aa0dbb51c26d8ba
Jakub Karczewski <karczewskij@student.agh.edu.pl>
2025-11-19 11:54:36 +0100

    Changed comments, added template for stirling

--- 2D arrays and recursion/other/stirling_and_number_splitting.py
+++ 2D arrays and recursion/other/stirling_and_number_splitting.py
@@ -1,7 +1,6 @@
-N = 10 # number to be splitted
-K = 3 # number of components
+K = 10 # maximum number of components, used only for initializing 'splitted' array
 
-splitted = [-1 for _ in range(N)]
+splitted = [-1 for _ in range(K)]
 last_idx = 0
 
 global count
@@ -30,12 +29,15 @@ def split_number(n, k, curr_num, last_idx, initial_k): # initial_k will not chan
     if k * curr_num > n: # If it is impossible to form number n using k numbers that are greater of equal to curr_num
         return
     
-    split_number(n, k, curr_num+1, last_idx, initial_k) # skip checking current number
+    split_number(n, k, curr_num+1, last_idx, initial_k) # skip checking current number,
+    # if we switch this line with the for loop, the splittings will be printed from smallest to greatest lexicographically
 
     for i in range(1, k+1): # take current number from 1 to k times
         splitted[last_idx + (i - 1)] = curr_num
         split_number(n - i * curr_num, k-i, curr_num + 1, last_idx + i, initial_k) # recursion for number greater by 1
 
+def stirling(n, k, last_idx, initial_k, add_to_nonempty:list[int]):
+    pass
 
 for N1, K1 in [(23, 5), (17, 4), (11, 3), (7, 2)]:
     print("-" * 100)
